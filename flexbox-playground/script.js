1410cad87f0df974df43b4429d7b974d83a7f695
FabianGenell <fabian.genell@hyperisland.se>
2022-09-23 07:38:48 +0200

    created layout added basic functionality

---
+++ flexbox-playground/script.js
@@ -0,0 +1,37 @@
+let controllerEl = document.getElementById('controller');
+let flexContainerEl = document.getElementById('flex-container');
+
+let children = 0;
+
+//on change get id of element changed and that id will be the flex-boc property and can be used to update that property.
+controllerEl.addEventListener('change', (e) => {
+
+    console.log(`Updated ${e.target.id} to ${e.target.value}`);
+
+    flexContainerEl.style[e.target.id] = e.target.value;
+
+});
+
+function createFlexChild(container, amount) {
+    for (let index = 0; index < amount; index++) {
+        children++;
+
+        let child = document.createElement('div');
+
+        child.classList.add('flex-child');
+        child.id = 'flex-child-' + index;
+        container.appendChild(child); //flexContainerEl.appendChild(child);
+
+
+
+
+    }
+
+}
+
+
+createFlexChild(flexContainerEl, 3);
+console.log(`Created ${children} flex items`);
+
+
+
